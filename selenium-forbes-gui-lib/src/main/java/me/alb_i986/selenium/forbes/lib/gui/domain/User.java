98d4640324ec13719112cfc46c87f33a5ba368d5
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-07-06 19:22:49 +0100

    Refactoring User
    - adding 'role' as a field
    - only one constructor, parameterless
    - adding `withField` methods for setting the other fields

--- selenium-forbes-gui-lib/src/main/java/me/alb_i986/selenium/forbes/lib/gui/domain/User.java
+++ selenium-forbes-gui-lib/src/main/java/me/alb_i986/selenium/forbes/lib/gui/domain/User.java
@@ -4,6 +4,7 @@ import me.alb_i986.selenium.forbes.lib.gui.pages.HomePage;
 
 /**
  * A User has a Browser.
+ * A User may have an email, and also a role.
  * 
  * @author alb
  *
@@ -12,15 +13,16 @@ public class User {
 
 	private Browser browser;
 	private String email = "";
+	private String role = "";
 
-	
+
+	/**
+	 * Create a User with nothing but a Browser.
+	 * Then, you may also want to set the other fields:
+	 * see with methods, e.g. {@link #withEmail(String)}.
+	 */
 	public User() {
 		this.browser = new Browser();
-	}
-	
-	public User(String email) {
-		this();
-		this.email = email;
 	}
 
 
@@ -36,6 +38,9 @@ public class User {
 		return browser.startSession();
 	}
 	
+	/**
+	 * @see Browser#close()
+	 */
 	public void closeBrowser() {
 		browser.close();
 	}
@@ -44,4 +49,25 @@ public class User {
 	public String getEmail() {
 		return email;
 	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public User withEmail(String email) {
+		if(email == null)
+			this.email = "";
+		else
+			this.email = email;
+		return this;
+	}
+
+	public User withRole(String role) {
+		if(role == null)
+			this.role = "";
+		else
+			this.role = role;
+		return this;
+	}
+
 }
