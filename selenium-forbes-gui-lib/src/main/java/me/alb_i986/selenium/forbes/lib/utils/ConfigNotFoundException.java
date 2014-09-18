3981cd8556c66b2734c406f3445ec72f81ba0a65
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-09-18 01:18:22 +0100

    implementing a util class for loading configs.
    It extracts props from 3 sources:
    - command line
    - custom config file
    - default config file

---
+++ selenium-forbes-gui-lib/src/main/java/me/alb_i986/selenium/forbes/lib/utils/ConfigNotFoundException.java
@@ -0,0 +1,10 @@
+package me.alb_i986.selenium.forbes.lib.utils;
+
+public class ConfigNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6017026647523692805L;
+
+}
